1f8659412281578a9b0a8713b4fc8bcf93cb68d6
ivan <ielizalde.dev@gmail.com>
2024-08-10 21:07:03 +0200

    rename components to pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @Component({
   selector: 'app-root',
@@ -10,5 +10,5 @@ import { HomeComponent } from './components/home/home.component';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'tailwind';
+  title = 'MatchUp';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
-import { EventViewComponent } from './components/event-view/event-view.component';
+import { HomeComponent } from './pages/home/home.component';
+import { EventViewComponent } from './pages/event-view/event-view.component';
 
 export const routes: Routes = [
     {path: '', component:HomeComponent},
