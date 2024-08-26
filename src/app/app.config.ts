a7500a60e64bd87fd3eb2cb06421dd71834f0fc9
ivan <ielizalde.dev@gmail.com>
2024-08-26 18:45:38 +0200

    firebase config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,21 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
+
+import { environment } from '../environments/environment.development'; 
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideDatabase(() => getDatabase()),
+  ],
 };
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebaseConfig: {
+    apiKey: 'AIzaSyCDzpWgmqohV01C12JoiSxEz9Lle59tDEE',
+    authDomain: 'matchup-d09b5.firebaseapp.com',
+    projectId: 'matchup-d09b5',
+    storageBucket: 'matchup-d09b5.appspot.com',
+    messagingSenderId: '445740416048',
+    appId: '1:445740416048:web:6beb01263ecb7b51c741f5',
+  },
+  production: true,
+};
