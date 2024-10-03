edf4e35feebda059dbbfe2d9d0b40bd4a45dbdcf
ivan <ielizalde.dev@gmail.com>
2024-10-04 01:34:04 +0200

    fix card styling

--- src/app/pages/home/components/event-grid/components/event-card/event-card.component.ts
+++ src/app/pages/home/components/event-grid/components/event-card/event-card.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { Event } from '../../../../../../shared/models/event.interface';
 import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-event-card',
   standalone: true,
-  imports: [MatIconModule],
+  imports: [MatIconModule, CommonModule],
   templateUrl: './event-card.component.html',
   styleUrl: './event-card.component.scss'
 })
