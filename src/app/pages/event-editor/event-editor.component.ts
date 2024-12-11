4c45c621ba9a39e3a7385c46d7d74cede6c00e35
ivan <ielizalde.dev@gmail.com>
2024-12-11 22:39:55 +0100

    implement drag on drop feature for uploading event images

--- src/app/pages/event-editor/event-editor.component.ts
+++ src/app/pages/event-editor/event-editor.component.ts
@@ -10,36 +10,46 @@ import { NavbarComponent } from '../../shared/components/navbar/navbar.component
 import { AuthService } from '../../shared/services/auth.service';
 import { HeaderComponent } from "../../shared/components/header/header.component";
 
+import { NgxDropzoneModule } from 'ngx-dropzone';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment.development';
+
 @Component({
   selector: 'app-event-editor',
   standalone: true,
-  imports: [ReactiveFormsModule, CommonModule, RouterModule, NavbarComponent, HeaderComponent],
+  imports: [ReactiveFormsModule, CommonModule, RouterModule, HeaderComponent, NgxDropzoneModule],
   templateUrl: './event-editor.component.html',
-  styleUrl: './event-editor.component.scss',
+  styleUrl: './event-editor.component.scss'
 })
 export class EventEditorComponent implements OnInit {
   eventForm = this._fb.group({
     id: [null],
     name: ['', Validators.required],
     description: ['', Validators.required],
-    image: [''], // Validators.required
+    image: [''],
     organizer: [''],
     organizerId: ['', Validators.required],
     date: ['', Validators.required],
     location: ['', Validators.required],
     price: [0, Validators.required],
     totalSpots: [0, Validators.required],
-    going: [0],
+    going: [0]
   });
 
   isEditing = false;
+
+  filePreview: string | null = null;
+
+  uploadedFiles: File[] = [];
+  uploadedImageUrl: string | null = null;
 
   constructor(
     private _fb: FormBuilder,
     private _eventService: EventService,
     private _authService: AuthService,
     private _route: ActivatedRoute,
-    private _router: Router
+    private _router: Router,
+    private http: HttpClient
   ) {}
 
   ngOnInit(): void {
@@ -51,30 +61,26 @@ export class EventEditorComponent implements OnInit {
       this._eventService
         .getEvent$()
         .pipe(
-          filter((v) => !!v),
+          filter(v => !!v),
           take(1)
         )
-        .subscribe((event) => {
+        .subscribe(event => {
           this.initForm(event);
         });
     } else {
       this._authService.user$.pipe(take(1)).subscribe(user => {
-        console.log(user)
+        console.log(user);
         this.eventForm.controls.organizer.setValue(user.email.split('@')[0]);
-        this.eventForm.controls.organizerId.setValue(user.uid)
-      })
+        this.eventForm.controls.organizerId.setValue(user.uid);
+      });
     }
-
-
   }
 
   initForm(event: Event): void {
     this.eventForm.patchValue({ ...event });
-    // this._cdr.detectChanges();
   }
 
   saveEvent(): void {
-    
     console.log(this.eventForm.value);
     if (this.eventForm.invalid) {
       return;
@@ -82,7 +88,8 @@ export class EventEditorComponent implements OnInit {
 
     const eventData: Event = {
       ...this.eventForm.value,
-      id: this.eventForm.value.id ?? ''
+      id: this.eventForm.value.id ?? '',
+      image: this.uploadedImageUrl || this.eventForm.value.image
     } as Event;
 
     if (this.isEditing) {
@@ -91,23 +98,73 @@ export class EventEditorComponent implements OnInit {
         this._eventService.loadEvents(true);
         this._eventService.loadEventById(eventData.id!, true);
         this._router.navigate(['/', 'event', eventData.id]);
-        
-        
       });
-    } else {      
-      
+    } else {
       this._eventService.updateEvent(eventData).then(() => {
-        
         this._eventService.loadEvents(true);
-        this._eventService.getEventsList$().pipe(
-          filter((v) => !!v?.find((e) => e.id === eventData.id)),
-          take(1)
-        ).subscribe(()=> {
-          console.log('Event created');
-          this._router.navigate(['/' ,'event' , eventData.id]);
-        });
-    } );
-  }
+        this._eventService
+          .getEventsList$()
+          .pipe(
+            filter(v => !!v?.find(e => e.id === eventData.id)),
+            take(1)
+          )
+          .subscribe(() => {
+            console.log('Event created');
+            this._router.navigate(['/', 'event', eventData.id]);
+          });
+      });
+    }
   }
 
+  onFileSelect(event: any) {
+    const file = event.addedFiles[0];
+    if (file) {
+      this.filePreview = null;
+      this.generateFilePreview(file);
+      this.validateAndUploadFile(file);
+    }
+  }
+
+  generateFilePreview(file: File) {
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.filePreview = reader.result as string;
+    };
+    reader.readAsDataURL(file);
+  }
+
+  validateAndUploadFile(file: File) {
+    const maxSize = 5000000;
+    const allowedTypes = ['image/jpeg', 'image/png', 'image/jpg'];
+
+    if (!allowedTypes.includes(file.type)) {
+      alert('Invalid file type. Only JPEG and PNG are allowed.');
+      return;
+    }
+
+    if (file.size > maxSize) {
+      alert('File is too large. Maximum size is 5 MB.');
+      return;
+    }
+
+    this.uploadFileToCloudinary(file);
+  }
+
+  uploadFileToCloudinary(file: File) {
+    const formData = new FormData();
+    formData.append('file', file);
+    formData.append('upload_preset', environment.cloudinary.uploadPreset);
+
+    this.http.post(`https://api.cloudinary.com/v1_1/${environment.cloudinary.cloudName}/image/upload`, formData).subscribe(
+      (response: any) => {
+        this.uploadedImageUrl = response.secure_url;
+        this.eventForm.patchValue({ image: this.uploadedImageUrl });
+        alert('Image uploaded successfully!');
+      },
+      error => {
+        console.error('Error uploading image:', error);
+        alert('Failed to upload image. Please try again.');
+      }
+    );
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,31 @@
-export const environment = {
+interface Environment {
+  firebaseConfig: {
+    apiKey: string;
+    authDomain: string;
+    projectId: string;
+    storageBucket: string;
+    messagingSenderId: string;
+    appId: string;
+  };
+  production: boolean;
+  cloudinary: {
+    cloudName: string;
+    uploadPreset: string;
+  };
+}
+
+export const environment: Environment = {
   firebaseConfig: {
     apiKey: 'AIzaSyCDzpWgmqohV01C12JoiSxEz9Lle59tDEE',
     authDomain: 'matchup-d09b5.firebaseapp.com',
     projectId: 'matchup-d09b5',
     storageBucket: 'matchup-d09b5.appspot.com',
     messagingSenderId: '445740416048',
-    appId: '1:445740416048:web:6beb01263ecb7b51c741f5',
+    appId: '1:445740416048:web:6beb01263ecb7b51c741f5'
   },
   production: false,
+  cloudinary: {
+    cloudName: 'dziphyjjx',
+    uploadPreset: 'MatchUp'
+  }
 };
